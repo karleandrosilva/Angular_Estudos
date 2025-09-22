f782b9232c59bc8098af3cf003e578b210d71c5a
karleandrosilva <contatoparakarle@gmail.com>
2025-09-22 10:56:13 -0300

    update aula 11

--- angular-basico/src/app/componente07/componente07.ts
+++ angular-basico/src/app/componente07/componente07.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componente07',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente07.html',
   styleUrl: './componente07.css'
 })
 export class Componente07 {
+
+  // Variavel logica
+  condicao : boolean = true;
+
+  // lista de aprovados e reprovados
+  lista : string[] = ['Aprovado', 'Reprovado', 'Aprovado', 'Reprovado'];
 
 }
