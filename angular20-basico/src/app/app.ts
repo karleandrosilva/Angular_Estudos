593f1e5408407326f00416ccf92726105b1793d2
karleandrosilva <contatoparakarle@gmail.com>
2025-12-08 11:21:42 -0300

    update aula 05

--- angular20-basico/src/app/app.ts
+++ angular20-basico/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponente } from "./primeiro-componente/primeiro-componente";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PrimeiroComponente],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
