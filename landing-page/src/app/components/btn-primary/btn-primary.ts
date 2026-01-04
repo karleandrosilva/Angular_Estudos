b6f5f215d71be88435ab02fcb62413473a1a1bf3
karleandrosilva <contatoparakarle@gmail.com>
2026-01-03 23:17:43 -0300

    update parte 2 project

--- landing-page/src/app/components/btn-primary/btn-primary.ts
+++ landing-page/src/app/components/btn-primary/btn-primary.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-btn-primary',
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class BtnPrimary {
 
+  // declarar um input
+  @Input("btn-text") btnText : string = ""; // começa vazio
+  @Input() disabled : boolean = false;
+  @Input() loading : boolean = false;
+  @Output("submit") onSubmit = new EventEmitter(); // onclick (do angular core)
+
+  // funcao submit = emite o evento 
+  submit() {
+    this.onSubmit.emit();
+  }
 }
+
+
+//1:38
