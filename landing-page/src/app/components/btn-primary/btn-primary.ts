0b33173d35341981562defe578c4269adae39235
karleandrosilva <contatoparakarle@gmail.com>
2026-01-11 19:34:25 -0300

    update parte 3.2 project

--- landing-page/src/app/components/btn-primary/btn-primary.ts
+++ landing-page/src/app/components/btn-primary/btn-primary.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-type BtnVariants = "primary" | "secondary"; // 
+type BtnVariants = "primary" | "secondary"; // Tipo que limita as variantes do botão
 
 @Component({
   selector: 'btn-primary',
@@ -8,19 +8,19 @@ type BtnVariants = "primary" | "secondary"; //
   templateUrl: './btn-primary.html',
   styleUrl: './btn-primary.scss',
 })
-export class BtnPrimary {
 
+export class BtnPrimary {
   // declarar um input
-  @Input("btn-text") btnText : string = ""; // começa vazio (para no html colocar o que quiser)
-  @Input() disabled : boolean = false; // tem que saber se tem disabled
-  @Input() loading : boolean = false;
-  @Input() variant : BtnVariants = "primary"  // variante para o botão
+  @Input("btn-text") btnText : string = ""; // começa vazio (para no html colocar o que quiser) - texto do botão
+  @Input() disabled : boolean = false; // Para saber se tem disabled -  define se o botão está desativado
+  @Input() loading : boolean = false; // Usado para trocar o texto por "carregando..." - define se o botão está em loading
+  @Input() variant : BtnVariants = "primary"  // variante para o botão - Só aceita "primary" ou "secondary"
 
   @Output("submit") onSubmit = new EventEmitter(); // onclick (do angular core)
 
-  // funcao submit = emite o evento 
+  // Método chamado quando o botão é clicado
   submit() {
-    this.onSubmit.emit();
+    this.onSubmit.emit(); // Dispara o evento para o componente pai
   }
 }
 
