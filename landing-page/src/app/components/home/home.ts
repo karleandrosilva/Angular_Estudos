a1a05530afe9a0484aae18020c7aa789fbaa7eee
karleandrosilva <contatoparakarle@gmail.com>
2026-01-15 21:04:15 -0300

    update parte 3.3 project

--- landing-page/src/app/components/home/home.ts
+++ landing-page/src/app/components/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import { NewsletterForm } from "../newsletter-form/newsletter-form";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, NgOptimizedImage, BtnPrimary],
+  imports: [Header, NgOptimizedImage, BtnPrimary, NewsletterForm],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
