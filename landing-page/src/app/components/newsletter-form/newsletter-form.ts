29d3e09e144e9d0e7f9a931058f8d8c3f6fccf54
karleandrosilva <contatoparakarle@gmail.com>
2026-01-15 21:03:45 -0300

    update parte 3.3 project

---
+++ landing-page/src/app/components/newsletter-form/newsletter-form.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BtnPrimary } from "../btn-primary/btn-primary";
+
+@Component({
+  selector: 'app-newsletter-form',
+  imports: [BtnPrimary],
+  templateUrl: './newsletter-form.html',
+  styleUrl: './newsletter-form.scss',
+})
+export class NewsletterForm {
+
+  
+}
