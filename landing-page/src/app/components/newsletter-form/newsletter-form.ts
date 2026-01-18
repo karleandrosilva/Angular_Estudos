f7f6f32ada9e9a51a3c2f5fd67d82f2a1f0e777a
karleandrosilva <contatoparakarle@gmail.com>
2026-01-17 21:17:21 -0300

    update parte 4 project

--- landing-page/src/app/components/newsletter-form/newsletter-form.ts
+++ landing-page/src/app/components/newsletter-form/newsletter-form.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
-  imports: [BtnPrimary],
+  imports: [BtnPrimary, ReactiveFormsModule],
   templateUrl: './newsletter-form.html',
   styleUrl: './newsletter-form.scss',
 })
 export class NewsletterForm {
+
+  // declaração do atributo forms
+  newsletterForm!: FormGroup;
+  
+  // construtor da classe
+  // criar formGroup (Coleção de inputs)
+  constructor() {
+    this.newsletterForm = new FormGroup({
+      
+      // formControl (forma de controlar o input)
+      name : new FormControl('',[Validators.required]),
+      email : new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+
+
+
+
 
   
 }
