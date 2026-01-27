f58852a644bd17d5c47a79a9a39b899ec12b0f99
karleandrosilva <contatoparakarle@gmail.com>
2026-01-26 21:17:29 -0300

    update aula 23 - modelo

---
+++ angular20-basico/src/app/modelo/Pessoa.ts
@@ -0,0 +1,10 @@
+// criar um modelo
+export class Pessoa {
+
+    // 3 atributos
+    // o type pede para inicializar as variaveis, então usa a interrogação
+    nome? : string;
+    idade? : number;
+    cidade? : string;
+
+}
