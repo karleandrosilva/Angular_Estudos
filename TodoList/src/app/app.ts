cf01e27857764cd3073379420c2cd84435974fdf
karleandrosilva <contatoparakarle@gmail.com>
2026-05-02 12:51:27 -0300

    update projeto todolist parte 1/5

--- TodoList/src/app/app.ts
+++ TodoList/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+// formatação das tarefas (interface do item)
+export interface TodoItem {
+  id : number;
+  task : string ;
+  completed : boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('TodoList');
+  todoList : TodoItem [] = []; // array para ser salvo
+  newTask : string = '';
 }
