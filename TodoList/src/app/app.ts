84f74468ac6bab6d29c55c8ace715a2074115bda
karleandrosilva <contatoparakarle@gmail.com>
2026-06-21 22:16:00 -0300

    update projeto todolist parte final

--- TodoList/src/app/app.ts
+++ TodoList/src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { NgClass } from '@angular/common';
 
 // formatação das tarefas (interface do item)
@@ -21,6 +20,23 @@ export class App {
   todoList : TodoItem [] = []; // array para ser salvo
   newTask : string = '';
 
+  // para as tarefas não sumir quando atualizar a página
+  ngOnInit() { // roda automaticamanete assim que o componente nasce
+
+    // localStorage = mini bd do navegador (guarda as info)
+    const saved = localStorage.getItem('todoList'); // vai até o mini bd do navegador e procura se existe algo guardado com o nome 'todoList'
+
+    // se encontrar alguma coisa
+    if(saved) {
+
+      // O navegador guarda tudo como TEXTO puro. 
+      // O 'JSON.parse()' transforma esse texto de volta em uma lista (array) de objetos TypeScript.
+      // Depois, joga a lista para dentro do 'this.todoList' para ela aparecer na tela.
+
+      this.todoList = JSON.parse(saved); 
+    }
+  }
+
   // funcionalidade de adicionar a task
   addTask() : void {
 
@@ -34,6 +50,9 @@ export class App {
       this.todoList.push(newTodoItem)
       this.newTask = '' // para deixar vazio, após pressionar o botão
     }
+
+    // JSON.stringify(): Pega a lista de objetos e transforma em uma "linha de texto" (uma String)
+    localStorage.setItem('todoList', JSON.stringify(this.todoList)); // para sempre salvar as tarefas quando adicionamos ou deletamos no localStorage
   }
 
   // funcionalidade da lista das tarefas, após adicionar uma tak
@@ -41,12 +60,14 @@ export class App {
   // componente do checkbox (para saber que esta completo)
   toggleCompleted(index:number) : void {
     this.todoList[index].completed = !this.todoList[index].completed // logica do completed
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
   }
 
   deleteTask(id:number) : void {
 
     this.todoList =this.todoList.filter(item => item.id != id)
     console.log(this.todoList)
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
   }
   
 }
